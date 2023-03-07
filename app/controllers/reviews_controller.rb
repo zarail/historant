ce83c462fa6e726554174ded0187fad5fe0eda91
Latif Turaan <latiftoran@gmail.com>
2023-03-07 17:55:27 +0100

    review journey

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,15 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @menu_item = MenuItem.find(params[:menu_item_id])
+    @review = Review.new
+  end
+
   def create
-    @reviews = Review.new(review_params)
-    review.menu_item = @menu_item
-    if reviews.save?
+    @menu_item = MenuItem.find(params[:menu_item_id])
+    @review = Review.new(review_params)
+    @review.menu_item = @menu_item
+    if @review.save!
       redirect_to menu_item_path(@menu_item)
     else
       render :new, status: :unprocessable_entity
@@ -12,6 +19,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(description, visit_date, rating)
+    params.require(:review).permit(:description, :visit_date, :rating)
   end
 end
