742f06823a9c7f68d440ba2259f894cd3806a88c
Latif Turaan <latiftoran@gmail.com>
2023-03-07 16:24:26 +0100

    review journey

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,17 @@
 class ReviewsController < ApplicationController
   def create
-    @reviews = Review.new(review-params)
+    @reviews = Review.new(review_params)
+    review.menu_item = @menu_item
     if reviews.save?
-      redirect-@reviews
+      redirect_to menu_item_path(@menu_item)
     else
-      render :new, :status unporcessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
+  def review_params
+    params.require(:review).permit(description, visit_date, rating)
+  end
 end
