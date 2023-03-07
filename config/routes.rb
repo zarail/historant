5fab85b56e4d08d17b6774ba3fc8ce40d4dddf18
Zahra Ilkhan <zahraa1991@gmail.com>
2023-03-07 12:41:36 +0100

    add all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resources :places, only: %i[new create] # index
+  end
+
+  resources :places, only: %i[destroy show] do # edit update
+    resources :menu_items, only: %i[new create] # index
+  end
+
+  resources :menu_items, only: %i[destroy show] do # edit update
+    resources :reviews, only: %i[new create] # index
+  end
+
+  resources :reviews, only: %i[destroy show] # edit update
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
