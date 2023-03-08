245ac5f79a3d56c36c604b0dc5f089a41e1caff4
Zahra Ilkhan <zahraa1991@gmail.com>
2023-03-08 12:48:34 +0100

    delete reviews table and merge columns to menu items

--- db/migrate/20230307104337_create_menu_items.rb
+++ db/migrate/20230307104337_create_menu_items.rb
@@ -4,6 +4,9 @@ class CreateMenuItems < ActiveRecord::Migration[7.0]
       t.string :category
       t.string :name
       t.float :price
+      t.date :visit_date
+      t.integer :rating
+      t.text :description
       t.references :place, null: false, foreign_key: true
 
       t.timestamps
