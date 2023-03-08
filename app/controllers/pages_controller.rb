f26a0fd4d5affee574e8711e09daf7a717681c3c
Latif Turaan <latiftoran@gmail.com>
2023-03-08 16:32:19 +0100

    dashboard add

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @places = current_user.places
+    @menu_items = current_user.menu_items
+  end
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,6 @@
 class MenuItem < ApplicationRecord
   belongs_to :place
+  has_one :user, through: :place
 
   validates :name, presence: true
 end
